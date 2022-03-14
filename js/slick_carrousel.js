538cc0bc37a7b8a7008cbf25f90a002809bab4a5
Carlos K. Portillo <carloskportillo@gmail.com>
2022-03-14 14:28:11 -0600

    Página Home Preliminar

---
+++ js/slick_carrousel.js
@@ -0,0 +1,14 @@
+$(function (){
+    $('.autoplay').slick({
+    slidestoShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+    arrows: false,
+    fade: true,
+    cssEase: 'linear',
+    centerMode: true,
+    centerPadding: true
+    });
+})
