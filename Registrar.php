927bd466f943c2246e762d9529bf86f48948ae92
Mirna11 <48633164+Mirna11@users.noreply.github.com>
2022-03-25 16:18:38 -0600

    Creación de registro

---
+++ Registrar.php
@@ -0,0 +1,27 @@
+<?php
+include("conexion.php");
+$con=conectar();
+
+$correo=$_POST['correo'];
+$clave=$_POST['pass'];
+$clave2=$_POST['pass2'];
+
+
+
+if($_POST["pass"]!=$_POST["pass2"]){
+    echo'<script type="text/javascript">
+    alert("Las claves no coinciden");
+    window.location.href="Registrarse.php";
+    </script>';
+}else{
+    $sql="INSERT INTO usuarios VALUES('','$correo','$clave')";
+    $query= mysqli_query($con,$sql);   
+    
+    if($query){
+        Header("Location: Login.php");
+        
+    }else {
+        echo "<h1>Intentelo de nuevo</h1>";
+    }
+}    
+?>
--- Registrarse.php
+++ Registrarse.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/registrarse.css">
     <link rel="stylesheet" href="css/slick.css">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>Huelic Restaurant</title>
+    <title>Registrarse Huelic Restaurant</title>
 </head>
 <body>
-    <div class="contain">
+<div class="contain">
         <header>
             <nav class="w-full mBrown flex items-center">
                 <div id="logosName" class="flex w-1/3">
@@ -19,7 +20,7 @@
                     </a>
                 </div>
                 <div id="navTabs" class="w-1/3 flex flex-row justify-center">
-                    <a href="" class="text-white text-xl font-semibold py-5 px-8">Home</a>
+                    <a href="index.html" class="text-white text-xl font-semibold py-5 px-8">Home</a>
                     <a href="" class="text-white text-xl font-semibold py-5 px-8">Menú</a>
                     <a href="" class="text-white text-xl font-semibold py-5 px-8">Reservación</a>
                     <a href="" class="text-white text-xl font-semibold py-5 px-8">Contacto</a>
@@ -35,49 +36,49 @@
                 </div>
             </nav>
         </header>
-        <section id="Hero">
-            <div class="w-full autoplay flex justify-center py-6">
-                <div class="">
-                    <img class="heroImage" src="resources/images/hero_table.jpg" alt="Hero 1">
+
+        <section class="flex mt-16 mb-16">
+            <div class="Izquierda h-full w-2/4 ml-12 mr-18">
+                <div class="flex justify-center pb-4">
+                   <img src="resources\images/huelic-white.png" class="img_logo w-1/4" alt=""> 
+                   <h2 class="text-4xl pl-4 pt-10 font-semibold"> Únetenos </h2>
                 </div>
-                <div class="">
-                    <img class="heroImage" src="resources/images/hero_table1.jpg" alt="Hero 2">
-                </div>
-                <div class="pb-3">
-                    <img class="heroImage" src="resources/images/hero_table2.jpg" alt="Hero 3">
-                </div>
+               <form action="Registrar.php" method="POST" class="pt-4 pl-16 pr-16">
+                    <div>
+                        <h3 class="font-semibold"> Correo Electronico: </h3>
+                    </div>
+                    <div class="mb-6">
+                        <input type="text" name="correo" class=" apperance-none block w-full px-4 py-2 leading-tight text-gray-700 bg-gray-50 focus:bg-white border border-gray-200 focus:border-gray-500 rounded focus:outline-none">
+                    </div>
+                    <div>
+                        <h3 class="font-semibold"> Contraseña: </h3>
+                    </div>
+                    <div class="mb-6">
+                        <input type="password" name="pass" class="apperance-none block w-full px-4 py-2 leading-tight text-gray-700 bg-gray-50 focus:bg-white border border-gray-200 focus:border-gray-500 rounded focus:outline-none" >
+                    </div>
+                    <div>
+                        <h3 class="font-semibold"> Repetir Contraseña: </h3>
+                    </div>
+                    <div class="mb-12">
+                        <input type="password" name="pass2" class="apperance-none block w-full px-4 py-2 leading-tight text-gray-700 bg-gray-50 focus:bg-white border border-gray-200 focus:border-gray-500 rounded focus:outline-none" >
+                    </div>
+                    <div class="mb-4">
+                        <input type="submit" value="Crear Cuenta" class="inline-block w-full px-8 py-3 leading-none text-white bg-amber-800 rounded hover:bg-amber-900 font-semibold shadow">
+                    </div>
+                </form>
             </div>
+            <div class="Derecha w-2/4 h-full ml-28 mr-12">
+                    <div class="pt-24 pl-20 w-3/4">
+                        <h3 class="font-semibold text-xl inline"> Tambien puedes conectarte con: </h3>
+                        <a href=""><img class="h-auto  mt-20 inline" src="resources/images/iniciargoogle.png" alt=""></a>
+                        <a href=""><img class="h-auto  mt-10 inline" src="resources/images/iniciarfacebook.png" alt=""></a>
+                    </div>
+            </div>   
         </section>
-        <section id="promises">
-            <div id="promisesContainer" class="w-11/12 flex bg-white mx-auto px-4 py-5 border-4 justify-around my-8">
-                <div class="w-1/4 flex py-2 items-center justify-around">
-                    <img class="promisesLogo" src="resources/images/checked.png" alt="Buena Calidad">
-                    <p class="text-center">
-                        Poseemos <br> la <br> mejor calidad
-                    </p>
-                </div>
-                <div class="w-1/4 flex py-2 items-center justify-around">
-                    <img class="promisesLogo" src="resources/images/thumbs-up.png" alt="Excelente Atención">
-                    <p class="text-center">
-                        La mejor atención <br> al <br> cliente
-                    </p>
-                </div>
-                <div class="w-1/4 flex py-2 items-center justify-around">
-                    <img class="promisesLogo" src="resources/images/gel.png" alt="Prótocolos de Salud">
-                    <p class="text-center">
-                        Protocolos <br> de <br> salud
-                    </p>
-                </div>
-            </div>
-        </section>
-        <section id="menu">
-            <div class="w-full my-24 py-8 flex justify-around">
-                <img class="menuLogo" src="resources/images/placeholder.png" alt="Food Placeholder">
-                <img class="menuLogo" src="resources/images/placeholder.png" alt="Food Placeholder">
-                <img class="menuLogo" src="resources/images/placeholder.png" alt="Food Placeholder">
-                <img class="menuLogo" src="resources/images/placeholder.png" alt="Food Placeholder">
-            </div>
-        </section>
+
+
+
+
         <footer class="w-full bg-black">
             <p class="font-semibold text-white text-xl text-center py-5">Si deseas ordenar a través de nuestro call center llama al (503) 2257-7777</p>
             <div class="w-full py-2 flex">
@@ -120,10 +121,7 @@
                 </div>
             </div>
         </footer>
-    </div>
-
-    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-    <script src="js/slick.min.js"></script>
-    <script src="js/slick_carrousel.js"></script>
+</div>
+    
 </body>
 </html>
